c3b8314c518a219820535d057901afcbe83d1a54
fariaregis2020 <fariaregis35@gmail.com>
2020-11-14 11:13:54 -0300

    User entity and resource

---
+++ src/main/java/com/iftm/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.iftm.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iftm.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+	  User u = new User(1l, "Maria", "maria@gmail.com", "321414900", "123456");
+	  return ResponseEntity.ok(u);
+	  
+		
+	}
+
+}
